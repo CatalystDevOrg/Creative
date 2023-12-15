3148bedc603e7522bf9eb95bfc2645541e0d1c09
jdev082 <jdev082@jdev.eu.org>
2023-12-15 13:41:31 -0600

    feat: fixes and new features

--- paint/index.js
+++ paint/index.js
@@ -2,20 +2,22 @@ const ws = document.getElementById("workspace");
 const wsctx = ws.getContext("2d");
 const cls = document.getElementById("brush_cl")
 const pos = document.getElementById("pos")
+const ipsize = document.getElementById("psize")
+pscale = 10;
 
 function draw(e) {
     wsctx.fillStyle = "black";
-    wsctx.fillRect(e.offsetX, e.offsetY, 10, 10);
+    wsctx.fillRect(e.offsetX, e.offsetY, pscale, pscale);
     console.log("DEBUG", e.offsetX, e.offsetY);
 }
 
-cursorX = Number.NaN;
-cursorY = Number.NaN;
+// cursorX = Number.NaN;
+// cursorY = Number.NaN;
 
 ws.onmousemove = function(event){
     cursorX = event.offsetX;
     cursorY = event.offsetY;
-    pos.innerText = `X: ${cursorX}, Y: ${cursorY}`
+    pos.innerText = `X: ${cursorX}, Y: ${cursorY},`
 }
 
 cls.addEventListener('change', function() {
@@ -24,4 +26,14 @@ cls.addEventListener('change', function() {
     cls.style.color = cl;
 })
 
+addEventListener("wheel", (e) => {
+    pscale = pscale + e.deltaY / 100
+    ipsize.innerText = `Scale: ${pscale}`
+});
+
 ws.onclick = draw;
+
+ws.width = window.innerWidth;
+ws.height = window.innerHeight;
+
+ipsize.innerText = `Scale: ${pscale}`;
